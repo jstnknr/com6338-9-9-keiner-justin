3103d356f22835b76cc695cc5adbdc75879e6c8f
jstnknr <143372221+jstnknr@users.noreply.github.com>
2024-04-01 12:50:47 -0400

    Assignment

--- index.js
+++ index.js
@@ -12,61 +12,117 @@
     //time
 
 
-    var weatherDiv = document.getElementById('weather')
-    var form = document.querySelector('form')
-    var inputValue = document.getElementById('weather-search')
+    const weatherDiv = document.getElementById('weather')
+    const form = document.querySelector('form')
+    const inputValue = document.getElementById('weather-search')
     
-    form.onsubmit = function(e) {
+    form.onsubmit = async function(e) {
         e.preventDefault()
-        fetch('https://api.openweathermap.org/data/2.5/weather?q='+inputValue.value+'&appid=78dc242a8f55c5581e2315885ba9ac52&units=imperial')
-        .then(function(res){
-            if(res.status !== 200) {
-                throw new Error('Location not found')
-            }
-            return res.json()
+        if (!inputValue) return
+        try {
+            const res = await fetch('https://api.openweathermap.org/data/2.5/weather?q='+inputValue.value+'&appid=78dc242a8f55c5581e2315885ba9ac52&units=imperial')
+            if(res.status !== 200) throw new Error('Location not found')
+            const weatherData = await res.json()
+            renderWeather(weatherData)
+        } catch(err) {
+         weatherDiv.innerHTML = err.message
+        }
+    }
+/*
+        const renderWeather = ({
+                name,
+                sys:{
+                    country
+                },
+                coord:{
+                    lat,
+                    lon
+                },
+                weather:{
+                    [0]:{
+                      icon,
+                      description
+                    }
+                },
+                main:{
+                    temp,
+                    feels_like
+            }  
+        
         })
-        .then(function(data) {
+        data => {
             weatherDiv.innerHTML = ""
             console.log(data)
-            var h2 = document.createElement('h2')
+            const h2 = document.createElement('h2')
+            h2.textContent = name+','+sys.country
+            weatherDiv.appendChild(h2)
+    
+            const link = document.createElement('a')
+            link.href = 'https://www.google.com/maps/search/?api=1&query='+coord.lat+','+coord.lon
+            link.textContent = 'click to view map'
+            weatherDiv.appendChild(link)
+    
+    
+            const weatherIcon = document.createElement('img')
+            weatherIcon.src = 'https://openweathermap.org/img/wn/'+icon+'@2x.png'
+            weatherDiv.appendChild(weatherIcon)
+    
+            const currentCondition = document.createElement('p')
+            currentCondition.textContent = description
+            weatherDiv.appendChild(currentCondition)
+    
+            const currentTemp = document.createElement('p')
+            currentTemp.textContent = 'Current: ' +temp+'° F'
+            weatherDiv.appendChild(currentTemp)
+    
+            const feelsTemp = document.createElement('p')
+            feelsTemp.textContent = 'Feels like: ' +feels_like+'° F'
+            weatherDiv.appendChild(feelsTemp)
+    
+            const lastUpdated = document.createElement('p')
+            lastUpdated.textContent = 'Last Updated: '+new Date().toLocaleTimeString()
+            weatherDiv.appendChild(lastUpdated)
+    
+        }
+    
+     
+    
+*/
+
+        const renderWeather = data => {
+            weatherDiv.innerHTML = ""
+            console.log(data)
+            const h2 = document.createElement('h2')
             h2.textContent = data.name +','+data.sys.country
             weatherDiv.appendChild(h2)
     
-            var link = document.createElement('a')
+            const link = document.createElement('a')
             link.href = 'https://www.google.com/maps/search/?api=1&query='+data.coord.lat+','+data.coord.lon
             link.textContent = 'click to view map'
             weatherDiv.appendChild(link)
     
     
-            var weatherIcon = document.createElement('img')
-            //weatherIcon.src = 'https://openweathermap.org/img/wn/'+data.weather[0]+'.png'
-            //weatherIcon.src = 'https://openweathermap.org/img/wn/02d@2x.png'
-            weatherIcon.src = 'https://openweathermap.org/img/wn/'+data.weather[0].icon+'@2x.png'
-            //weatherIcon.alt = currentCondition
-    
+            const weatherIcon = document.createElement('img')
+            weatherIcon.src = 'https://openweathermap.org/img/wn/'+data.weather[0].icon+'@2x.png'    
             weatherDiv.appendChild(weatherIcon)
     
-            var currentCondition = document.createElement('p')
+            const currentCondition = document.createElement('p')
             currentCondition.textContent = data.weather[0].description
             weatherDiv.appendChild(currentCondition)
     
-            var currentTemp = document.createElement('p')
+            const currentTemp = document.createElement('p')
             currentTemp.textContent = 'Current: ' + data.main.temp+'° F'
             weatherDiv.appendChild(currentTemp)
     
-            var feelsTemp = document.createElement('p')
+            const feelsTemp = document.createElement('p')
             feelsTemp.textContent = 'Feels like: ' + data.main.feels_like+'° F'
             weatherDiv.appendChild(feelsTemp)
     
-            var lastUpdated = document.createElement('p')
+            const lastUpdated = document.createElement('p')
             lastUpdated.textContent = 'Last Updated: '+new Date().toLocaleTimeString()
             weatherDiv.appendChild(lastUpdated)
     
-        })
+        }
     
-        .catch(function(err){
-            weatherDiv.innerHTML = err.message
-        })
-    }
      
     
